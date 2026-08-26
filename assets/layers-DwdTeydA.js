9aef1ed6b50af91d74806d6a811918a334c4359a
farik9797 <farrukh.abdumadjidov@gmail.com>
2026-08-26 15:19:22 +0500

    Deploy da8aee1

--- assets/layers-DwdTeydA.js
+++ assets/layers-DwdTeydA.js
@@ -1,4 +1,4 @@
-import{c as a}from"./index-Bm4GhmYy.js";/**
+import{c as a}from"./index-Cn-XBS42.js";/**
  * @license lucide-react v0.546.0 - ISC
  *
  * This source code is licensed under the ISC license.
