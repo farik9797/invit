31e27e450306830746980a240d9af57034418d7e
farik9797 <farrukh.abdumadjidov@gmail.com>
2026-08-27 08:59:07 +0500

    Deploy 6c81189

--- assets/AboutOldPage-BwYiOuD9.js
+++ assets/AboutOldPage-BwYiOuD9.js
@@ -1,4 +1,4 @@
-import{c as n,r as c,j as e,y as r,Z as x,q as o,$ as b,K as m,a as h}from"./index-BSxMZh9w.js";import{c as d}from"./contentImages-DFQLuo-d.js";import{L as p}from"./layers-C8KOH83D.js";/**
+import{c as n,r as c,j as e,y as r,Z as x,q as o,$ as b,K as m,a as h}from"./index-BBwMrZDW.js";import{c as d}from"./contentImages-8_w8dKOA.js";import{L as p}from"./layers-D9kVnteX.js";/**
  * @license lucide-react v0.546.0 - ISC
  *
  * This source code is licensed under the ISC license.
