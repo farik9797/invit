833e6db65f19d0833ace6a4bea5747a1ec1abbfb
farik9797 <farrukh.abdumadjidov@gmail.com>
2026-08-27 13:57:18 +0500

    Deploy 0230ba7

--- assets/ProductPage-DpsgUDQF.js
+++ assets/ProductPage-DpsgUDQF.js
@@ -1,4 +1,4 @@
-import{c as w,j as e,D as C,q as S,P as v,E as P,r as c,_,G as E,a as x,o as I,I as T,J as q,K as D,L as j,Q as L,x as k,w as Q,U as $,V as M,W as O,Y as A}from"./index-CbV03oeF.js";import{c as R,p as U}from"./contentImages-DxVLkVff.js";import{S as V}from"./shield-check-kgAnbyDd.js";/**
+import{c as w,j as e,D as C,q as S,P as v,E as P,r as c,_,G as E,a as x,o as I,I as T,J as q,K as D,L as j,Q as L,x as k,w as Q,U as $,V as M,W as O,Y as A}from"./index-DTi_0vCv.js";import{c as R,p as U}from"./contentImages-CMDv7IBs.js";import{S as V}from"./shield-check-BGyE9M__.js";/**
  * @license lucide-react v0.546.0 - ISC
  *
  * This source code is licensed under the ISC license.
