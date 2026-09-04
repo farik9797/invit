6f46c0942f87aae0c744f4ad9c7e1419aed13782
farik9797 <farrukh.abdumadjidov@gmail.com>
2026-09-04 10:02:10 +0500

    Deploy 37e939b

--- assets/ProductPage-CdehSbDe.js
+++ assets/ProductPage-CdehSbDe.js
@@ -1,4 +1,4 @@
-import{c as w,j as e,D as C,y as S,P as v,E as P,r as c,_,G as E,a as x,o as I,I as T,J as D,K as L,L as j,Q,w as k,v as $,U as q,V as M,W as O,Y as A}from"./index-BmMshfME.js";import{c as R,p as U}from"./contentImages-DVYINiNU.js";import{S as V}from"./shield-check-C5wFxUWa.js";/**
+import{c as w,j as e,D as C,y as S,P as v,E as P,r as c,_,G as E,a as x,o as I,I as T,J as D,K as L,L as j,Q,w as k,v as $,U as q,V as M,W as O,Y as A}from"./index-fWkQRoMt.js";import{c as R,p as U}from"./contentImages-DOpN6NCa.js";import{S as V}from"./shield-check-BZtUUuYK.js";/**
  * @license lucide-react v0.546.0 - ISC
  *
  * This source code is licensed under the ISC license.
--- assets/shield-check-BZtUUuYK.js
+++ assets/shield-check-BZtUUuYK.js
@@ -1,4 +1,4 @@
-import{c}from"./index-BmMshfME.js";/**
+import{c}from"./index-fWkQRoMt.js";/**
  * @license lucide-react v0.546.0 - ISC
  *
  * This source code is licensed under the ISC license.
