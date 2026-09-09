89140ab964ebdd3735c2c428756a7a5cd6877751
farik9797 <farrukh.abdumadjidov@gmail.com>
2026-09-09 17:28:52 +0500

    Deploy e738f56

--- assets/layers-DPMa-m8d.js
+++ assets/layers-DPMa-m8d.js
@@ -1,4 +1,4 @@
-import{c as a}from"./index-DcxUbN3u.js";/**
+import{c as a}from"./index-R16LbmeO.js";/**
  * @license lucide-react v0.546.0 - ISC
  *
  * This source code is licensed under the ISC license.
