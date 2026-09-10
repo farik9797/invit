81e63f1d8bd4f932345ff84b4e924f70b541c564
farik9797 <farrukh.abdumadjidov@gmail.com>
2026-09-10 15:04:58 +0500

    Deploy d499c64

--- assets/AboutOldPage-IIOKnbXz.js
+++ assets/AboutOldPage-IIOKnbXz.js
@@ -1,4 +1,4 @@
-import{c as n,r as c,j as e,x as r,$ as x,y as o,a0 as b,K as m,a as h}from"./index-DihlaG3x.js";import{c as d}from"./contentImages-D6gKiVzX.js";import{L as p}from"./layers-BT9gR6vE.js";/**
+import{c as n,r as c,j as e,x as r,$ as x,y as o,a0 as b,K as m,a as h}from"./index-Dd4oZuV7.js";import{c as d}from"./contentImages-BaHd7MCB.js";import{L as p}from"./layers-DcCyGRSS.js";/**
  * @license lucide-react v0.546.0 - ISC
  *
  * This source code is licensed under the ISC license.
