30713c465c154e830652f15bcc8a59d46076d9b9
farik9797 <farrukh.abdumadjidov@gmail.com>
2026-09-23 15:46:56 +0500

    Deploy 7341dda

--- assets/AboutOldPage-DtUvCBtj.js
+++ assets/AboutOldPage-DtUvCBtj.js
@@ -1,4 +1,4 @@
-import{c as n,r as c,j as e,x as r,a2 as x,y as o,a3 as b,Q as m,a as h}from"./index-ClOMY1V4.js";import{c as d}from"./contentImages-BE2AFYbq.js";import{L as p}from"./layers-C3c3AEZJ.js";/**
+import{c as n,r as c,j as e,x as r,a2 as x,y as o,a3 as b,Q as m,a as h}from"./index-H8MHC-jB.js";import{c as d}from"./contentImages-2y7wdse9.js";import{L as p}from"./layers-Bot4w82X.js";/**
  * @license lucide-react v0.546.0 - ISC
  *
  * This source code is licensed under the ISC license.
--- assets/shield-check-PLDWyPOY.js
+++ assets/shield-check-PLDWyPOY.js
@@ -1,4 +1,4 @@
-import{c}from"./index-ClOMY1V4.js";/**
+import{c}from"./index-H8MHC-jB.js";/**
  * @license lucide-react v0.546.0 - ISC
  *
  * This source code is licensed under the ISC license.
