02e5afe5b8f358d80bffc9ad9f7062af0e411ff1
farik9797 <farrukh.abdumadjidov@gmail.com>
2026-09-24 17:06:58 +0500

    Deploy 2a7bb9e

--- assets/AboutOldPage-DQD0t_Og.js
+++ assets/AboutOldPage-DQD0t_Og.js
@@ -1,4 +1,4 @@
-import{c as n,r as c,j as e,x as r,a2 as x,y as o,a3 as b,Q as m,a as h}from"./index-AiBqx-YT.js";import{c as d}from"./contentImages-BLD85m34.js";import{L as p}from"./layers-5pxDsQnV.js";/**
+import{c as n,r as c,j as e,x as r,a2 as x,y as o,a3 as b,Q as m,a as h}from"./index-DhZT2yeh.js";import{c as d}from"./contentImages-CY9Y47st.js";import{L as p}from"./layers-D8VG5zgd.js";/**
  * @license lucide-react v0.546.0 - ISC
  *
  * This source code is licensed under the ISC license.
--- assets/layers-D8VG5zgd.js
+++ assets/layers-D8VG5zgd.js
@@ -1,4 +1,4 @@
-import{c as a}from"./index-AiBqx-YT.js";/**
+import{c as a}from"./index-DhZT2yeh.js";/**
  * @license lucide-react v0.546.0 - ISC
  *
  * This source code is licensed under the ISC license.
