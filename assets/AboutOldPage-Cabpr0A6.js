2e33febf2aa920aa8418979c397d0aa2f9d55549
farik9797 <farrukh.abdumadjidov@gmail.com>
2026-09-24 17:20:34 +0500

    Deploy 3264e63

--- assets/AboutOldPage-Cabpr0A6.js
+++ assets/AboutOldPage-Cabpr0A6.js
@@ -1,4 +1,4 @@
-import{c as n,r as c,j as e,x as r,a2 as x,y as o,a3 as b,Q as m,a as h}from"./index-ChwtUSeT.js";import{c as d}from"./contentImages-CO6pj-Fe.js";import{L as p}from"./layers-04kQ0Wdm.js";/**
+import{c as n,r as c,j as e,x as r,a2 as x,y as o,a3 as b,Q as m,a as h}from"./index-BrvPQ1mo.js";import{c as d}from"./contentImages-B2739eXu.js";import{L as p}from"./layers-0K6NnWG8.js";/**
  * @license lucide-react v0.546.0 - ISC
  *
  * This source code is licensed under the ISC license.
