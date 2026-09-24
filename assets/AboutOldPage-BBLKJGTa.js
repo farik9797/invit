213e1581c1db09402ff9c7c391253b48cdcfa978
farik9797 <farrukh.abdumadjidov@gmail.com>
2026-09-24 09:21:58 +0500

    Deploy 17a7e35

--- assets/AboutOldPage-BBLKJGTa.js
+++ assets/AboutOldPage-BBLKJGTa.js
@@ -1,4 +1,4 @@
-import{c as n,r as c,j as e,x as r,a2 as x,y as o,a3 as b,Q as m,a as h}from"./index-RADtEKHj.js";import{c as d}from"./contentImages-DEoBHkAm.js";import{L as p}from"./layers-CokMHN3p.js";/**
+import{c as n,r as c,j as e,x as r,a2 as x,y as o,a3 as b,Q as m,a as h}from"./index-C6sixpox.js";import{c as d}from"./contentImages-Crx1ZfIb.js";import{L as p}from"./layers-BNilQzns.js";/**
  * @license lucide-react v0.546.0 - ISC
  *
  * This source code is licensed under the ISC license.
--- assets/LayoutV2-7MHCj7yG.js
+++ assets/LayoutV2-7MHCj7yG.js
@@ -1,4 +1,4 @@
-import{c as j,j as e,X as o,k as g,v as N,w as v,r as n,x as w,S as k,y,z as C,H as S,O as A,o as q,a as E,q as O,s as P}from"./index-RADtEKHj.js";import{L as V}from"./layers-CokMHN3p.js";import{S as L}from"./shield-check-CMF075NA.js";/**
+import{c as j,j as e,X as o,k as g,v as N,w as v,r as n,x as w,S as k,y,z as C,H as S,O as A,o as q,a as E,q as O,s as P}from"./index-C6sixpox.js";import{L as V}from"./layers-BNilQzns.js";import{S as L}from"./shield-check-CR3r5A9r.js";/**
  * @license lucide-react v0.546.0 - ISC
  *
  * This source code is licensed under the ISC license.
